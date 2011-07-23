5974e89ea5808ad4cb9d273a80277b670eb1ae86
Patrick Bajao <earl.bajao@gmail.com>
2011-07-23 18:27:19 +0800

    Refactored matcher_spec to use JobWithArgs.

--- spec/rspec-resque/matchers_spec.rb
+++ spec/rspec-resque/matchers_spec.rb
@@ -11,9 +11,9 @@ describe "RspecResque Matchers" do
     end
     
     context "with arguments" do
-      before { Resque.enqueue(Job, 0, 10) }
+      before { Resque.enqueue(JobWithArgs, 0, 10) }
     
-      subject { Job }
+      subject { JobWithArgs }
     
       it { should have_queued(0, 10) }
       it { should_not have_queued(10, 0) }
@@ -22,12 +22,12 @@ describe "RspecResque Matchers" do
   
   describe "#have_queue_size_of" do
     before do
-      Resque.enqueue(Job, 0, 10)
-      Resque.enqueue(Job, 11, 20)
-      Resque.enqueue(Job, 21, 30)
+      Resque.enqueue(JobWithArgs, 0, 10)
+      Resque.enqueue(JobWithArgs, 11, 20)
+      Resque.enqueue(JobWithArgs, 21, 30)
     end
     
-    subject { Job }
+    subject { JobWithArgs }
     
     it { should have_queue_size_of(3) }
     it { should_not have_queue_size_of(1) }
--- spec/support/jobs.rb
+++ spec/support/jobs.rb
@@ -7,3 +7,13 @@ class Job
     self.runs += 1
   end
 end
+
+class JobWithArgs
+  @queue = :queue
+  
+  attr_accessor :runs
+  
+  def self.perform(start, finish)
+    self.runs += 1
+  end
+end
