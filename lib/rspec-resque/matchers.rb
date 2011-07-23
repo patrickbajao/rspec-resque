96299732cf043f748fc83d385b2be873b268865b
Patrick Bajao <earl.bajao@gmail.com>
2011-07-23 18:18:40 +0800

    Added have_queue_size_of matcher.

--- lib/rspec-resque/matchers.rb
+++ lib/rspec-resque/matchers.rb
@@ -1,8 +1,12 @@
 require "rspec"
 
 module MatcherHelper
+  def queue(klass)
+    Resque.peek(Resque.queue_from_class(klass).to_s, 0, 0)
+  end
+  
   def enqueued?(klass, expected_args)
-    Resque.peek(Resque.queue_from_class(klass).to_s, 0, 0).any? { |entry| entry["class"].to_s == klass.to_s && entry['args'] == expected_args }
+    queue(klass).any? { |entry| entry["class"].to_s == klass.to_s && entry['args'] == expected_args }
   end
 end
 
@@ -23,5 +27,25 @@ RSpec::Matchers.define :have_queued do |*expected_args|
 
   description do
     "have queued arguments of [#{expected_args.join(', ')}]"
+  end
+end
+
+RSpec::Matchers.define :have_queue_size_of do |size|
+  extend MatcherHelper
+
+  match do |actual|
+    queue(actual).size == size
+  end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual} would have #{size} entries queued, but got #{queue(actual).size} instead"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected that #{actual} would not have #{size} entries queued, but got #{queue(actual).size} instead"
+  end
+
+  description do
+    "have a queue size of #{size}"
   end
 end
--- spec/rspec-resque/matchers_spec.rb
+++ spec/rspec-resque/matchers_spec.rb
@@ -19,4 +19,17 @@ describe "RspecResque Matchers" do
       it { should_not have_queued(10, 0) }
     end
   end
+  
+  describe "#have_queue_size_of" do
+    before do
+      Resque.enqueue(Job, 0, 10)
+      Resque.enqueue(Job, 11, 20)
+      Resque.enqueue(Job, 21, 30)
+    end
+    
+    subject { Job }
+    
+    it { should have_queue_size_of(3) }
+    it { should_not have_queue_size_of(1) }
+  end
 end
