51a3f270ea408ad982a67f4276f38b7f2eb9215a
Patrick Bajao <earl.bajao@gmail.com>
2011-07-23 16:42:29 +0800

    Refactored how #have_queued matcher checks if the job is enqueued.

--- lib/rspec-resque/matchers.rb
+++ lib/rspec-resque/matchers.rb
@@ -1,8 +1,16 @@
 require "rspec"
 
+module MatcherHelper
+  def enqueued?(klass, expected_args)
+    Resque.peek(Resque.queue_from_class(klass).to_s, 0, 0).any? { |entry| entry["class"].to_s == klass.to_s && entry['args'] == expected_args }
+  end
+end
+
 RSpec::Matchers.define :have_queued do |*expected_args|
+  extend MatcherHelper
+  
   match do |actual|
-    Resque.peek(Resque.queue_from_class(actual).to_s, 0, 0).any? { |entry| entry["class"].to_s == actual.to_s && entry['args'] == expected_args }
+    enqueued?(actual, expected_args)
   end
 
   failure_message_for_should do |actual|
