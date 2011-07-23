28c856c27f342e521605c47ae493e41f9c7cb1a4
Patrick Bajao <earl.bajao@gmail.com>
2011-07-23 23:53:56 +0800

    Added test for job hooks.

--- spec/rspec-resque/helpers_spec.rb
+++ spec/rspec-resque/helpers_spec.rb
@@ -7,5 +7,18 @@ describe RspecResque::Helpers do
       Resque.enqueue(Job)
       run_resque(:queue)
     end
+    
+    context "job hooks" do
+      before do
+        Resque.enqueue(JobWithHooks)
+        run_resque(:queue)
+      end
+      
+      [:after_enqueue, :before_perform, :after_perform, :around_perform].each do |hook|
+        it "should perform #{hook.to_s} hook" do
+          Resque.redis.keys("#{hook.to_s}").should have_exactly(1).item
+        end
+      end
+    end
   end
 end
--- spec/support/jobs.rb
+++ spec/support/jobs.rb
@@ -17,3 +17,29 @@ class JobWithArgs
     self.runs += 1
   end
 end
+
+class JobWithHooks
+  @queue = :queue
+  
+  attr_accessor :runs
+  
+  def self.after_enqueue(*args)
+    Resque.redis.setnx("after_enqueue", 1)
+  end
+  
+  def self.before_perform(*args)
+    Resque.redis.setnx("before_perform", 1)
+  end
+  
+  def self.after_perform(*args)
+    Resque.redis.setnx("after_perform", 1)
+  end
+  
+  def self.around_perform(*args)
+    Resque.redis.setnx("around_perform", 1)
+  end
+  
+  def self.perform
+    self.runs += 1
+  end
+end
